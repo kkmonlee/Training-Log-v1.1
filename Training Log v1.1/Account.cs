31d285f9f98857d5b0a409b85dc04eb27bab47e9
kkmonlee <kkmonlee@gmail.com>
2015-05-01 10:04:57 +0100

    Calling instances and making strings
    Starting to build up

---
+++ Training Log v1.1/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Log_v1._1
+{
+    class Account
+    {
+    }
+}
---
+++ Training Log v1.1/Login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Log_v1._1
+{
+    class Login
+    {
+        private string userName;
+        private string password;
+
+        public Login(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
